29f0e59b43169385653a0593b6e778971057aa81
jluismartinezram@gmail.com <jluismartinezram@gmail.com>
2025-01-17 23:18:27 -0600

    advanced function finished

--- 01_tutorials/03_advanced_function.R
+++ 01_tutorials/03_advanced_function.R
@@ -12,7 +12,7 @@
 library(tidyverse)
 
 ## Functions
-source("...")
+source("R/util.R")
 
 # 2. Load data ------------------------------------------------------------
 
@@ -22,7 +22,8 @@ iris_tbl <- as_tibble(iris)
 # 3. Analyze data ---------------------------------------------------------
 
 ## Calculate mean of numeric variables in Iris dataset, and sort them
-
+iris_tbl |> 
+   calc_iris_mean()
 
 
 # 4. General function -----------------------------------------------------
@@ -30,8 +31,42 @@ iris_tbl <- as_tibble(iris)
 ## https://ggplot2.tidyverse.org/reference/tidyeval.html
 
 ## Apply to other datasets
+## FUNCION: 
+calc_numeric_mean <- function(data, group) {
+    data |> 
+        summarise(
+            across(
+                where(is.numeric), \(x) mean(x, na.rm = TRUE)
+            ),
+            .by = {{ group }}  
+        ) |> 
+        pivot_longer(
+            cols = where(is.numeric),
+            names_to = "measure",
+            values_to = "mean"
+        ) |> 
+        arrange(
+            desc(mean)
+        ) |> 
+        group_by(measure) |> 
+        arrange(
+            desc(mean),
+            .by_group = TRUE
+        ) |> 
+        ungroup()
+}
 
+# Aplicarlo a otro conjunto de datos
 
+airquality |> 
+    calc_numeric_mean(
+        group = Month
+    )
+
+calc_numeric_mean(
+    data = iris_tbl,
+    group = Species
+)
 
 
 
--- R/util.R
+++ R/util.R
@@ -17,3 +17,107 @@ calculo_volumen <- function(diametro, altura){
     pi / 4 * (diametro/100)^2 * altura
 }
 
+
+##  FUNCION: resumo conjunto de datos de iris
+calc_iris_mean <- function(data){
+    data |> 
+        summarise(
+            across(
+                where(is.numeric), mean
+            ),
+            .by = Species
+        ) |> 
+        pivot_longer(
+            cols = where(is.numeric),
+            names_to = "measure",
+            values_to = "mean"
+        ) |> 
+        arrange(
+            desc(mean)
+        ) |> 
+        group_by(measure) |> 
+        arrange(
+            desc(mean),
+            .by_group = TRUE
+        ) |> 
+        ungroup()
+}
+
+
+## FUNCION: Resumir una media de variables numericas 
+calc_numeric_mean <- function(data, group) {
+    data |> 
+        summarise(
+            across(
+                where(is.numeric), \(x) mean(x, na.rm = TRUE)
+            ),
+            .by = {{ group }}  
+        ) |> 
+        pivot_longer(
+            cols = where(is.numeric),
+            names_to = "measure",
+            values_to = "mean"
+        ) |> 
+        arrange(
+            desc(mean)
+        ) |> 
+        group_by(measure) |> 
+        arrange(
+            desc(mean),
+            .by_group = TRUE
+        ) |> 
+        ungroup()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+calc_numeric_mean <- function(data, group) {
+    data |> 
+        summarise(
+            across(
+                where(is.numeric), \(x) mean(x, na.rm = TRUE)
+            ),
+            .by = {{ group }}  
+        ) |> 
+        pivot_longer(
+            cols = where(is.numeric),
+            names_to = "measure",
+            values_to = "mean"
+        ) |> 
+        arrange(
+            desc(mean)
+        ) |> 
+        group_by(measure) |> 
+        arrange(
+            desc(mean),
+            .by_group = TRUE
+        ) |> 
+        ungroup()
+}
+
+
+
+
+
+
+
