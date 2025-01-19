e86a5ea02101061f7b022c76bb055f02dac041ab
jluismartinezram@gmail.com <jluismartinezram@gmail.com>
2025-01-19 12:57:00 -0600

    finished purr tutorial

--- 01_tutorials/04_purrr.R
+++ 01_tutorials/04_purrr.R
@@ -26,9 +26,20 @@ objects_list <- list("peach", "pear", "cherry", "strawberry", "blackberry")
 ## 3.1. Using for loop ---------------------------
 
 ## Create an empty list
+messages_list <- list()
 
+for (i in 1:length(objects_list)){
+    
+    messages_list[[i]] <- str_glue("This is a {objects_list[[i]]}")
+    
+}
 
 ## 3.2. Using functional programming -------------
+
+messages_list <- map(
+    objects_list,
+    \(fruit) str_glue("This fruit is a {fruit}")
+) 
 
 
 # 4. Model by species -----------------------------------------------------
@@ -44,26 +55,65 @@ species_vec <- unique(iris_tbl$Species)
 filtered_lst <- list()
 
 ## Iterate to filter each species
+for (species in 1:length(species_vec)){
+    
+    ## filtrar observaciones
+    iris_filtered <- iris_tbl |> 
+        filter(
+            Species == species_vec[species]
+        )
+    ## Calcular un modelo lineal 
+    iris_filtered_lm <- lm(Petal.Length ~ Sepal.Length, data = iris_filtered)
+    ## Obteniendo una sumaria
+    filtered_list [[species]] <- summary(iris_filtered_lm)
+}
 
-
+filtered_list
+    
+    
 ## 4.2. Using functional programming --------------
 
 ## Create a function
-
+calculate_iris_lm <- function(data){
+    
+    lm(Petal.Length ~ Sepal.Length, data = data) |> 
+        summary()
+}
 
 ## Create list
-
+iris_species_list <- iris_tbl |> 
+    split(iris_tbl$Species)
 
 ## Iterate over the list
+map(
+    iris_species_list,
+    calculate_iris_lm
+)
+
+map(
+    iris_species_list,
+    \(x) calculate_iris_lm(x)
+)
 
 
 ## 4.3. In one step ---------------------------------
+iris_tbl |> 
+    split(iris_tbl$Species) |> 
+    map(calculate_iris_lm)
 
 
 ## 4.4. Using anonymous function --------------------
+iris_tbl |> 
+    split(iris_tbl$Species) |> 
+    map(
+        \(data) lm(Petal.Length ~ Sepal.Length, data = data) |> 
+            summary()
+    )
 
 
 # 5. Iterating 2 inputs --------------------------------------------------
+
+
 
 ## 5.1. Simple example ----------------------------
 
@@ -72,19 +122,64 @@ fruits <- list("peach", "pear", "cherry", "strawberry", "blackberry")
 colors <- list("orange", "green", "red", "red", "black")
 
 ## Iterate to create the sentence
+map2_chr(
+    .x = fruits,
+    .y = colors,
+    \(fruit, color) str_glue("El color de {fruit} es {color}")
+) 
+
 
 
 ## 5.2. A bigger example --------------------------
 
 ## Create a grid of parameters
-
+params_tbl <- expand_grid(
+    ntree = c(100, 200, 500, 1000),
+    mtry = 1:4
+)
 
 ## Create a Random Forest model for each parameter
+iris_rf_list <- map2(
+    .x = params_tbl$ntree,
+    .y = params_tbl$mtry,
+    \(ntree, mtry) ranger(
+        formula = Sepal.Length ~ . , 
+        data      = iris_tbl,
+        num.trees = ntree,
+        mtry      = mtry
+    )
+)
 
 
 ## Extract r.squared
-
+map(
+    iris_rf_list,
+    \(rf_model) rf_model$r.squared
+)
 
 ## Add as a new column
+params_tbl |> 
+    mutate(
+        rsq = map_dbl(
+            iris_rf_list,
+            \(rf_model) rf_model$r.square
+        )
+    ) |> 
+    arrange(desc(rsq))
+
+## vamos a sumar otro vector para complemento 
+rounded <- c("rounded", "not rounded", "rounded", "not rounded", "rounded")
+
+## iterar sobre 3 vectores 
+pmap(
+    .l = list(
+        fruits, 
+        colors,
+        rounded
+    ),
+    \(fruit, color, rounded) str_glue("El color de {fruit} es {color} y es {rounded}")
+)
+
+
 
 
